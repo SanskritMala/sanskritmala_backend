e3c5fe57b446f44b3330d4265c60d208eead5973
VipulSingh1407 <vipulsingh9369384252@gmail.com>
2024-10-30 13:14:45 +0530

    notes free

--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { isAuth } from '../middlewares/isAuth.js';
-import { getAllNotes, getMyNotes, getNotePdf, getSingleNote, noteCheckout, notesVerification } from '../controllers/notes.js';
+import { accessNote, getAllNotes, getMyNotes, getNotePdf, getSingleNote } from '../controllers/notes.js';
 
 
 const router = express.Router();
@@ -9,7 +9,5 @@ router.get('/notes/all', getAllNotes);
 router.get('/notes/:id',getSingleNote);
 router.get('/mynotes',isAuth,getMyNotes);
 router.get("/notes/:id/pdf", isAuth, getNotePdf);
-router.post("/notes/checkout/:id",isAuth, noteCheckout);
-router.post("/notesverification/:id",isAuth,notesVerification);
-
+router.post('/notes/access/:id',isAuth,accessNote)
 export default router;
